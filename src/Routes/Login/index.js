e667a0169c64298bc5f71402e79480c2841a2576
Qudusayo <zeekimono@gmail.com>
2020-11-15 10:46:16 +0100

    Changed login

--- src/Routes/Login/index.js
+++ src/Routes/Login/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./style.module.scss";
 
 function index() {
-    return <div>Login</div>;
+    return <div className={styles.login}>Login</div>;
 }
 
 export default index;

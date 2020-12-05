7b0ef327ce87d199c6b72a17cace600ba06e0421
Qudusayo <zeekimono@gmail.com>
2020-12-05 22:59:40 +0100

    Added confirmation +1

--- src/Routes/Transfer/index.js
+++ src/Routes/Transfer/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import swalt from "@sweetalert/with-react";
 import Swal from "sweetalert2";
 import Wrapper from "./../../Components/Container";
 
@@ -39,58 +40,83 @@ class index extends Component {
     }
 
     onSubmit = (e) => {
-        this.setState({ waiting: true });
         e.preventDefault();
         const data = {
             amount: this.state.amount,
             reciever: this.state.reciever,
-            type: "Transfer Fund"
+            type: "Transfer Fund",
         };
         const api = `${process.env.REACT_APP_BACKEND_URI}/sendMoney`;
         const token = JSON.parse(sessionStorage.getItem("topuplab")).token;
-        axios
-            .post(api, data, {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`,
-                },
-            })
-            .then((res) => {
-                console.log(res.data);
-                if (!res.data.error) {
-                    Swal.fire({
-                        title: "Transaction Completed",
-                        text: "Money transferred successfully!",
-                        icon: "success",
-                        position: "top-end",
-                        showConfirmButton: false,
-                        timer: 3000,
+
+        if (!data.amount || !data.reciever) {
+            return swalt(
+                "Transfer Failed",
+                "Invalid transfer details",
+                "error"
+            );
+        } else if (data.amount < 50) {
+            return swalt("Transfer Failed", "Amount less than ₦50", "error");
+        }
+
+        Swal.fire({
+            title: "Verify Transfer",
+            text: "You won't be able to revert this!",
+            html: `<div><p style="display:flex;">Amount:-- <b>₦${this.state.amount}</b></p><p style="display:flex;">Reciever:-- <b>${this.state.reciever}</b></p></div>`,
+            icon: "question",
+            backdrop: "#00000090",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Transfer",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                this.setState({ waiting: true });
+                axios
+                    .post(api, data, {
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${token}`,
+                        },
+                    })
+                    .then((res) => {
+                        console.log(res.data);
+                        if (!res.data.error) {
+                            Swal.fire({
+                                title: "Transaction Completed",
+                                text: "Money transferred successfully!",
+                                icon: "success",
+                                position: "top-end",
+                                showConfirmButton: false,
+                                timer: 3000,
+                            });
+                            this.setState({
+                                amount: "",
+                                reciever: "",
+                            });
+                            this.props.history.push("/dashboard");
+                        } else {
+                            Swal.fire({
+                                icon: "error",
+                                title: "Transaction Failed",
+                                text: `${res.data.message}`,
+                                position: "top-end",
+                                showConfirmButton: false,
+                                timer: 3000,
+                            });
+                            this.setState({ waiting: false });
+                        }
+                    })
+                    .catch((err) => {
+                        Swal.fire({
+                            icon: "error",
+                            title: "Oops...",
+                            text: "Something went wrong!",
+                        });
+                        this.setState({ waiting: false });
                     });
-                    this.setState({
-                        amount: "",
-                        reciever: "",
-                    });
-                    this.props.history.push("/dashboard");
-                } else {
-                    Swal.fire({
-                        icon: "error",
-                        title: "Transaction Failed",
-                        text: `${res.data.message}`,
-                        position: "top-end",
-                        showConfirmButton: false,
-                        timer: 3000,
-                    });
-                    this.setState({ waiting: false });
-                }
-            })
-            .catch((err) => {
-                Swal.fire({
-                    icon: "error",
-                    title: "Oops...",
-                    text: "Something went wrong!",
-                });
-                this.setState({ waiting: false });
-            });
+            }
+        });
     };
 
     render() {

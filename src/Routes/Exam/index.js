87a9f9f16553bcbf9e298e0834ee28352816a776
Qudusayo <zeekimono@gmail.com>
2020-12-06 18:38:26 +0100

    Updated exam route

--- src/Routes/Exam/index.js
+++ src/Routes/Exam/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import axios from "axios";
+import swalt from "@sweetalert/with-react";
+import Swal from "sweetalert2";
 import Wrapper from "./../../Components/Container";
 
-// import spinner from "./../../assets/images/logos/loading.png";
+import spinner from "./../../assets/images/logos/loading.png";
 
 import styles from "./../Transfer/style.module.scss";
 
@@ -10,35 +13,140 @@ class index extends Component {
         super(props);
 
         this.state = {
-            balance: "45,000",
-            reciever: "",
-            amount: "",
+            type: "",
+            quantity: "",
             waiting: false,
         };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
-    onChange() {}
+
+    onChange = (e) => {
+        this.setState({ [e.target.id]: e.target.value });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        const data = {
+            quantity: this.state.quantity,
+            type: this.state.type,
+            cost:
+                this.state.type.toLowerCase() === "waec"
+                    ? 1750 * this.state.quantity
+                    : 700 * this.state.quantity,
+        };
+        const api = `${process.env.REACT_APP_BACKEND_URI}/transaction/exam`;
+        const token = JSON.parse(sessionStorage.getItem("topuplab")).token;
+
+        if (!["waec", "neco"].includes(data.type.toLowerCase())) {
+            return swalt(
+                "Pin Purchase Failed",
+                "Invalid transaction details",
+                "error"
+            );
+        } else if (data.quantity < 1 || data.quantity > 5) {
+            return swalt(
+                "Pin Purchase Failed",
+                "Should be less or equal to  5 and greater or equal to 1",
+                "error"
+            );
+        }
+
+        Swal.fire({
+            title: "Verify Purchase",
+            text: "You won't be able to revert this!",
+            html: `<div><p style="display:flex;">Cost:-- <b>₦${
+                data.cost
+            }</b></p><p style="display:flex;">Exam type:-- <b>${data.type.toUpperCase()}</b></p> <p style="display:flex;">Quantity:-- <b>${
+                data.quantity
+            }</b></p></div>`,
+            icon: "question",
+            backdrop: "#00000090",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Purchase PIN",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                this.setState({ waiting: true });
+                axios
+                    .post(api, data, {
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${token}`,
+                        },
+                    })
+                    .then((res) => {
+                        console.log(res.data);
+                        if (!res.data.error) {
+                            const Toast = Swal.mixin({
+                                toast: true,
+                                position: "top-end",
+                                showConfirmButton: false,
+                                timer: 3000,
+                                timerProgressBar: true,
+                                didOpen: (toast) => {
+                                    toast.addEventListener(
+                                        "mouseenter",
+                                        Swal.stopTimer
+                                    );
+                                    toast.addEventListener(
+                                        "mouseleave",
+                                        Swal.resumeTimer
+                                    );
+                                },
+                            });
+
+                            Toast.fire({
+                                icon: "success",
+                                title: "PIN purchased successfully",
+                            });
+                            this.setState({
+                                quantity: "",
+                                type: "",
+                            });
+                            this.props.history.push("/dashboard");
+                        } else {
+                            swalt(
+                                "PIN Purchase Failed",
+                                `${res.data.errorMsg}`,
+                                "warning"
+                            );
+                            this.setState({ waiting: false });
+                        }
+                    })
+                    .catch((err) => {
+                        swalt(
+                            "PIN Purchase Failed",
+                            "Error  completing the transaction",
+                            "error"
+                        );
+                        this.setState({ waiting: false });
+                    });
+            }
+        });
+    };
+
     render() {
         return (
             <Wrapper>
-                <form className={styles.Form}>
+                <form className={styles.Form} onSubmit={this.onSubmit}>
                     <h1>EXAMINATION PIN</h1>
-                    <h3>COMING  SOON!!</h3>
+                    <h3>COMING SOON!!</h3>
                     <label>Examination Type</label>
                     <select
-                        name="networkProvider"
-                        id="networkProvider"
+                        name="type"
+                        id="type"
                         className={styles.networkProvider}
-                        required
+                        required={true}
+                        onChange={this.onChange}
                     >
                         <option value="" hidden>
                             Examination Type
                         </option>
-                        <option value="WAEC">
-                            WAEC (Cost ₦1750)
-                        </option>
-                        <option value="NECO">
-                            NECO (Cost ₦700)
-                        </option>
+                        <option value="WAEC">WAEC (Cost ₦1750)</option>
+                        <option value="NECO">NECO (Cost ₦700)</option>
                     </select>
                     <label>Quantity</label>
                     <input
@@ -51,8 +159,10 @@ class index extends Component {
                         value={this.state.quantity}
                         required={true}
                         disabled={this.state.waiting}
+                        min="1"
+                        max="5"
                     />
-                    {/* <button type="submit" disabled={true}>
+                    <button type="submit" disabled={this.state.waiting}>
                         {this.state.waiting ? (
                             <img
                                 className={styles.spinner}
@@ -60,9 +170,9 @@ class index extends Component {
                                 alt="spinner"
                             />
                         ) : (
-                            "COMING SOON"
+                            "REQUEST PIN"
                         )}
-                    </button> */}
+                    </button>
                 </form>
             </Wrapper>
         );

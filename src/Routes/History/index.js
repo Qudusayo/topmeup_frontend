b3712d6925f8243ccdd80795d7f0d0b083560c4b
Qudusayo <zeekimono@gmail.com>
2020-12-25 15:45:15 +0100

    Fixed responsiveness

--- src/Routes/History/index.js
+++ src/Routes/History/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import axios from "axios";
-import InfiniteScroll from "react-infinite-scroll-component";
 import { Helmet } from "react-helmet";
 import { connect } from "react-redux";
 import Wrapper from "./../../Components/Container";
@@ -33,7 +32,9 @@ class Index extends Component {
                     fullHistory: res.data,
                     history: res.data.slice(
                         0,
-                        res.data.length <= 10 ? res.data.length : this.state.quantity
+                        res.data.length <= 10
+                            ? res.data.length
+                            : this.state.quantity
                     ),
                     quantity:
                         res.data.length % 10 > 1 ? res.data.length + 10 : 10,
@@ -47,7 +48,9 @@ class Index extends Component {
             this.setState({
                 history: this.state.fullHistory.slice(
                     0,
-                    this.state.fullHistory.length <= 10 ? this.state.fullHistory.length : this.state.quantity
+                    this.state.fullHistory.length <= 10
+                        ? this.state.fullHistory.length
+                        : this.state.quantity
                 ),
                 quantity:
                     this.state.fullHistory.length % 10 > 1
@@ -69,70 +72,46 @@ class Index extends Component {
                 </form>
                 {this.state.loading ? (
                     <Loader />
+                ) : this.state.history.length ? (
+                    this.state.history.reverse().map((trans, index) => {
+                        return (
+                            <div
+                                className={[styles.card, styles.loose].join(
+                                    " "
+                                )}
+                                key={index}
+                            >
+                                <div className={[styles.infos].join(" ")}>
+                                    <h4>
+                                        {trans.type}{" "}
+                                        {trans.id ? (
+                                            <small>( {trans.id} )</small>
+                                        ) : null}
+                                    </h4>
+                                    <p>{trans.date}</p>
+                                </div>
+                                <h2>₦{parseFloat(trans.amount)}</h2>
+                                <div className={[styles.infos].join(" ")}>
+                                    <p>
+                                        <b>Topped Up:</b> {trans.reciever}
+                                    </p>
+                                    <p
+                                        className={styles.success}
+                                        style={{
+                                            backgroundColor: this.state[
+                                                trans.status
+                                            ],
+                                        }}
+                                    >
+                                        {trans.status[0].toUpperCase() +
+                                            trans.status.slice(1)}
+                                    </p>
+                                </div>
+                            </div>
+                        );
+                    })
                 ) : (
-                    <InfiniteScroll
-                        dataLength={this.state.history.length}
-                        next={this.fetchMoreData}
-                        hasMore={this.state.history.length < this.state.fullHistory.length}
-                        loader={<h4>Loading...</h4>}
-                    >
-                        {this.state.history.length
-                            ? this.state.history
-                                  .reverse()
-                                  .map((trans, index) => {
-                                      return (
-                                          <div
-                                              className={[
-                                                  styles.card,
-                                                  styles.loose,
-                                              ].join(" ")}
-                                              key={index}
-                                          >
-                                              <div
-                                                  className={[
-                                                      styles.infos,
-                                                  ].join(" ")}
-                                              >
-                                                  <h4>
-                                                      {trans.type}{" "}
-                                                      {trans.id ? (
-                                                          <small>
-                                                              ( {trans.id} )
-                                                          </small>
-                                                      ) : null}
-                                                  </h4>
-                                                  <p>{trans.date}</p>
-                                              </div>
-                                              <h2>
-                                                  ₦{parseFloat(trans.amount)}
-                                              </h2>
-                                              <div
-                                                  className={[
-                                                      styles.infos,
-                                                  ].join(" ")}
-                                              >
-                                                  <p>
-                                                      <b>Topped Up:</b>{" "}
-                                                      {trans.reciever}
-                                                  </p>
-                                                  <p
-                                                      className={styles.success}
-                                                      style={{
-                                                          backgroundColor: this
-                                                              .state[
-                                                              trans.status
-                                                          ],
-                                                      }}
-                                                  >
-                                                      {trans.status[0].toUpperCase() +
-                                                          trans.status.slice(1)}
-                                                  </p>
-                                              </div>
-                                          </div>
-                                      );
-                                  })
-                            : "No Transaction History"}
-                    </InfiniteScroll>
+                    "No Transaction History"
                 )}
             </Wrapper>
         );
